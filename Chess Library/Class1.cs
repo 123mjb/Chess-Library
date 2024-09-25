585de2c5a3b741b0272a350960a1abf7db01bdd9
123mjb <micahbutt@outlook.com>
2024-09-25 10:08:58 +0100

    Add project files.

---
+++ Chess Library/Class1.cs
@@ -0,0 +1,7 @@
+﻿string[] aaaa = new string[63];
+Array.Fill(aaaa, "a");
+
+Chess_Library.ChessBot Bot = new Chess_Library.ChessBot(aaaa,false);
+
+Bot.Move
+
---
+++ Chess Library/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Chess_Library
+{
+    internal class ChessBot{
+        private bool window;
+        private string[] boardLayout;
+        private readonly string nameinmoves = "KkNnQqRrBb";
+        public ChessBot(string[] pieces, bool testinterface = false)
+        {
+            if (pieces.Length != 64)
+            {
+                throw new ArgumentException("Pieces is not a standard chess board size of 64.");
+            }
+            window = testinterface;
+            boardLayout = pieces;
+        }
+        /// <summary>
+        /// Method <c>Move</c> tries to do the move supplied.
+        /// </summary>
+        /// <param name="move">The move in SAN</param>
+        /// <returns>Whether the move is valid or not.</returns>
+        public bool Move(string move)
+        {
+            if (!nameinmoves.Contains(move[0]))
+            {
+
+            }
+        }
+
+    }
+}
+
