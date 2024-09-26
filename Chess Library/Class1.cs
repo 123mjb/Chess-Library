26afed9089d76ae0fbdb90875f8102e940342ff5
123mjb <micahbutt@outlook.com>
2024-09-26 21:11:11 +0100

    Some more move interpretation

--- Chess Library/Class1.cs
+++ Chess Library/Class1.cs
@@ -3,5 +3,16 @@ Array.Fill(aaaa, "a");
 
 Chess_Library.ChessBot Bot = new Chess_Library.ChessBot(aaaa,false);
 
-Bot.Move
-
+/*if (Bot.Move("a4"))
+{
+    Console.WriteLine("Move was successful");
+}
+else
+{
+    Console.WriteLine("Move was not successful");
+}*/
+string b = "a2";
+if (b[2] > 1)
+{
+    Console.WriteLine("Move was successful");
+}
--- Chess Library/Program.cs
+++ Chess Library/Program.cs
@@ -1,14 +1,18 @@
 ﻿
 
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
 namespace Chess_Library
 {
     internal class ChessBot{
         private bool window;
         private string[] boardLayout;
-        private readonly string nameinmoves = "KkNnQqRrBb";
+        private bool PlayerMove;
 
         public ChessBot(string[] pieces, bool testinterface = false)
         {
+            PlayerMove = false;
             if (pieces.Length != 64)
             {
                 throw new ArgumentException("Pieces is not a standard chess board size of 64.");
@@ -23,6 +27,7 @@ namespace Chess_Library
         /// <returns>Whether the move is valid or not.</returns>
         public bool Move(string move)
         {
+
             switch(move[0].ToString())
             {
                 case "K":
@@ -34,6 +39,30 @@ namespace Chess_Library
                 default:
                     PawnMove(move);
                     break;
+            }
+            static int num(string ForR)
+            {
+                switch (ForR)
+                {
+                    case "a":
+                        return 1;
+                    case "b":
+                        return 2;
+                    case "c":
+                        return 3;
+                    case "d":
+                        return 4;
+                    case "e":
+                        return 5;
+                    case "f":
+                        return 6;
+                    case "g":
+                        return 7;
+                    case "h":
+                        return 8;
+                    default:
+                        return -1;
+                }
             }
             bool BishopMove(string move)
             {
@@ -57,21 +86,24 @@ namespace Chess_Library
             }
             bool PawnMove(string move)
             {
-                if (move.Contains(Convert.ToChar("x")))
-                {
-                    string[] movesplit = move.Split("x");
-                }
-                else
-                {
-                    if(move.Length > 2)
+                if (move.Contains(Convert.ToChar("="))){
+                    if (move.Contains(Convert.ToChar("x")))
                     {
-
+                        string[] movesplit = move.Split("x");
                     }
                     else
                     {
-                        if
+                        if (move.Length > 2)
+                        {
+
+                        }
+                        else
+                        {
+                            if (Convert.ToInt32(move[2]) > 1) { if (boardLayout[])}
+                        }
                     }
                 }
+                
                 if (move.Length > 2)
                 {
                     if (move.Contains(Convert.ToChar("x")))
