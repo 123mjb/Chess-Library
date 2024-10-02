a36827e111b6a3e67a176016bcb464e1b07a351b
123mjb <micahbutt@outlook.com>
2024-10-02 09:45:41 +0100

    making the move parsing better

--- Chess Library/Program.cs
+++ Chess Library/Program.cs
@@ -6,9 +6,29 @@
         private string[] boardLayout;
         private bool PlayerMove;
         private string[][] pieces = new string[6][] { new string[2] { "P", "p" }, new string[2] { "K", "k" }, new string[2] { "Q", "q" }, new string[2] { "N", "n" }, new string[2] { "B", "b" }, new string[2] { "R", "r" } };
+        int piecetonum(string peice)
+        {
+            switch (peice.ToLower())
+            {
+                case "p":
+                    return 1;
+                case "k":
+                    return 2;
+                case "q":
+                    return 3;
+                case "n":
+                    return 4;
+                case "b":
+                    return 5;
+                case "r":
+                    return 6;
+                default : return 0;
+
+            }
+        }
         string getmovepiece(int pieceNum)
         {
-            return pieces[pieceNum][PlayerMove ? 0 : 1];
+            return pieceNum[pieceNum][PlayerMove ? 0 : 1];
         }
         public ChessBot(string[] pieces, bool testinterface = false)
         {
@@ -86,37 +106,24 @@
             }
             bool PawnMove(string move)
             {
-                if (move.Contains(Convert.ToChar("=")))
-                {
-                    if (move.Contains(Convert.ToChar("x")))
-                    {
-                        string[] movesplit = move.Split("x");
-                    }
-                    else
-                    {
-                        if (move.Length > 2)
-                        {
-                            if (move.Contains(Convert.ToChar("=")))
-                            {
-
-                            }
-                        }
-                        else
-                        {
-                            if (Convert.ToInt32(move[2]) > 1)
-                            {
-                                return MoveCheckerAndDoer(8*num(move[0].ToString()) + Convert.ToInt32(move[2]) - 1, num(move[0].ToString()) + Convert.ToInt32(move[2]) - 1, 0,false);
-                            }
-                            else
-                            {
-                                return false;
-                            }
-                        }
-                    }
+                int from, to, piecenum,topiecenum;
+                bool take;
+                if (move.Contains(Convert.ToChar("="))){
+                    topiecenum = piecetonum(move.Split('=')[1]);
+                }
+                if (move.Contains(Convert.ToChar("x"))) {
+                    string[] sects = move.Split('x');
+                    sects[1] = sects[1].Split('=')[0];
+                    take = true;
                 }
 
+
+
+                return MoveCheckerAndDoer()
+                //if (move.Contains(Convert.ToChar("="))){if (move.Contains(Convert.ToChar("x"))){string[] movesplit = move.Split("x");}else{if (move.Length > 2){}else{if (Convert.ToInt32(move[2]) > 1){return MoveCheckerAndDoer(8*num(move[0].ToString()) + Convert.ToInt32(move[2]) - 1, num(move[0].ToString()) + Convert.ToInt32(move[2]) - 1, 0,false);}else{return false;}}}}
+
             }
-            bool MoveCheckerAndDoer(int from, int to, int piecenum, bool taking)
+            bool MoveCheckerAndDoer(int from, int to, int piecenum,int topiecenum, bool taking)
             {
                 if (!taking)
                 {
