1739714c2603772a0f36e5917a59a9ad178d4fb8
123mjb <micahbutt@outlook.com>
2025-01-20 15:05:55 +0000

    Bishop

--- Chess Library/BishopMovement.cs
+++ Chess Library/BishopMovement.cs
@@ -10,22 +10,22 @@ namespace Chess_Library
 
             MoveDetails actualmove = diagonallymove(chessBoard, editedmove[0], editedmove[1]);
         }
-        private static MoveDetails diagonallymove(ChessBoard chessBoard, string to, string from)
+        private static MoveDetails diagonallymove(ChessBoard chessBoard, string to, string from,bool Playermove)
         {
             if (NumForRank(from[0]) > NumForRank(to[0]))
             {
-                return checker(chessBoard, MoveNum(to), 1);
+                return checker(chessBoard, MoveNum(to), true);
             }
             else if (NumForRank(from[0]) < NumForRank(to[0]))
             {
-                return checker(chessBoard, MoveNum(to), -1);
+                return checker(chessBoard, MoveNum(to), false);
             }
             else return new MoveDetails(false);
         }
-        private static MoveDetails diagonallymove(ChessBoard chessBoard, string to)
+        private static MoveDetails diagonallymove(ChessBoard chessBoard, string to, bool Playermove)
         {
-            MoveDetails c1 = checker(chessBoard, MoveNum(to),1);
-            MoveDetails c2 = checker(chessBoard, MoveNum(to), -1);
+            MoveDetails c1 = checker(chessBoard, MoveNum(to),true);
+            MoveDetails c2 = checker(chessBoard, MoveNum(to), false);
             if (c1 & c2)
             {
                 return new MoveDetails(false);
@@ -36,17 +36,33 @@ namespace Chess_Library
             }
             return c2;
         }
-        private static MoveDetails checker(ChessBoard chessBoard, int from, int move)
+        private static MoveDetails checker(ChessBoard chessBoard, int from, bool move, bool Playermove)
         {
-            
+            MoveDetails c1 = up(chessBoard, from, move);
+            MoveDetails c2 = down(chessBoard, from, move);
+            if (c1 & c2)
+            {
+                return new MoveDetails(false);
+            }
+            else if (c1.Outcome)
+            {
+                return c1;
+            }
+            return c2;
         }
-        private static MoveDetails up(ChessBoard chessBoard, int from, int move)
+        private static MoveDetails up(ChessBoard chessBoard, int from, bool move, bool Playermove)
         {
-
+            for (int i=from; (chessBoard[i]==GetMovePiece(6,Playermove)|| chessBoard[i] == ""); i += move ? 7 : -9)
+            {
+                
+            }
         }
-        private static MoveDetails down(ChessBoard chessBoard, int from, int move)
+        private static MoveDetails down(ChessBoard chessBoard, int from, bool move, bool Playermove)
         {
+            for (int i = from; ; i += move ? 9 : -7)
+            {
 
+            }
         }
     }
 }
